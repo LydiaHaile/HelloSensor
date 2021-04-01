9e12757679d2833a0509fdc799df9f1ee7ebea8d
LydiaHaile <lydiaefrem@gmail.com>
2021-04-02 00:19:37 +0200

    code from internet

--- app/src/main/java/com/example/hellosensor/MainActivity.java
+++ app/src/main/java/com/example/hellosensor/MainActivity.java
@@ -1,14 +1,173 @@
 package com.example.hellosensor;
 
-import androidx.appcompat.app.AppCompatActivity;
+//import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+//import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+//public class MainActivity extends AppCompatActivity {
+
+    //@Override
+    //protected void onCreate(Bundle savedInstanceState) {
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+    //}
+//}
+
+//import android.hardware.Sensor;
+//import android.hardware.SensorManager;
+////import android.support.v7.app.AppCompatActivity;
+//import androidx.appcompat.app.AppCompatActivity;
+//import android.view.animation.Animation;
+//import android.view.animation.RotateAnimation;
+//import android.hardware.SensorEvent;
+//import android.widget.ImageView;
+//import android.widget.TextView;
+//import android.hardware.SensorEventListener;
+//import android.os.Bundle;
+//public class MainActivity extends AppCompatActivity implements SensorEventListener {
+//    // device sensor manager
+//    private SensorManager SensorManage;
+//    // define the compass picture that will be use
+//    private ImageView compassimage;
+//    // record the angle turned of the compass picture
+//    private float DegreeStart = 0f;
+//    TextView DegreeTV;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        //
+//        compassimage = (ImageView) findViewById(R.id.compass_image);
+//        // TextView that will display the degree
+//        DegreeTV = (TextView) findViewById(R.id.DegreeTV);
+//        // initialize your android device sensor capabilities
+//        SensorManage = (SensorManager) getSystemService(SENSOR_SERVICE);
+//    }
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        // to stop the listener and save battery
+//        SensorManage.unregisterListener(this);
+//    }
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        // code for system's orientation sensor registered listeners
+//        SensorManage.registerListener(this, SensorManage.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+//                SensorManager.SENSOR_DELAY_GAME);
+//    }
+//    @Override
+//    public void onSensorChanged(SensorEvent event) {
+//        // get angle around the z-axis rotated
+//        float degree = Math.round(event.values[0]);
+//        DegreeTV.setText("Heading: " + Float.toString(degree) + " degrees");
+//        // rotation animation - reverse turn degree degrees
+//        RotateAnimation ra = new RotateAnimation(
+//                DegreeStart,
+//                -degree,
+//                Animation.RELATIVE_TO_SELF, 0.5f,
+//                Animation.RELATIVE_TO_SELF, 0.5f);
+//        // set the compass animation after the end of the reservation status
+//        ra.setFillAfter(true);
+//        // set how long the animation for the compass image will take place
+//        ra.setDuration(210);
+//        // Start animation of compass image
+//        compassimage.startAnimation(ra);
+//        DegreeStart = -degree;
+//    }
+//    @Override
+//    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+//        // not in use
+//    }
+//}
+
+
+        import android.app.Activity;
+        import android.hardware.Sensor;
+        import android.hardware.SensorEvent;
+        import android.hardware.SensorEventListener;
+        import android.hardware.SensorManager;
+        import android.os.Bundle;
+        import android.view.animation.Animation;
+        import android.view.animation.RotateAnimation;
+        import android.widget.ImageView;
+        import android.widget.TextView;
+
+public class MainActivity extends Activity implements SensorEventListener {
+
+    // define the display assembly compass picture
+    private ImageView image;
+
+    // record the compass picture angle turned
+    private float currentDegree = 0f;
+
+    // device sensor manager
+    private SensorManager mSensorManager;
+
+    TextView tvHeading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // our compass image
+        image = (ImageView) findViewById(R.id.imageViewCompass);
+
+        // TextView that will tell the user what degree is he heading
+        tvHeading = (TextView) findViewById(R.id.tvHeading);
+
+        // initialize your android device sensor capabilities
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // for the system's orientation sensor registered listeners
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // to stop the listener and save battery
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        // get the angle around the z-axis rotated
+        float degree = Math.round(event.values[0]);
+
+        tvHeading.setText("Heading: " + Float.toString(degree) + " degrees");
+
+        // create a rotation animation (reverse turn degree degrees)
+        RotateAnimation ra = new RotateAnimation(
+                currentDegree,
+                -degree,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF,
+                0.5f);
+
+        // how long the animation will take place
+        ra.setDuration(210);
+
+        // set the animation after the end of the reservation status
+        ra.setFillAfter(true);
+
+        // Start the animation
+        image.startAnimation(ra);
+        currentDegree = -degree;
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // not in use
     }
 }
